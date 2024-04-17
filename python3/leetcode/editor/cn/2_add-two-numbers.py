b82f3cf68da5bef437715ed3663cd6c218bf300c
leihuazhe <295482300@qq.com>
2024-04-17 21:39:38 +0800

    108-3: Reserved, time:  2024-04-17 21:39:38

---
+++ python3/leetcode/editor/cn/2_add-two-numbers.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+from typing import Optional
+
+
+# source:  https://leetcode-cn.com/problems/add-two-numbers
+# source:  https://leetcode.com/problems/add-two-numbers
+
+# [2]-add-two-numbers
+
+# 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。 
+# 
+#  请你将两个数相加，并以相同形式返回一个表示和的链表。 
+# 
+#  你可以假设除了数字 0 之外，这两个数都不会以 0 开头。 
+# 
+#  
+# 
+#  示例 1： 
+#  
+#  
+# 输入：l1 = [2,4,3], l2 = [5,6,4]
+# 输出：[7,0,8]
+# 解释：342 + 465 = 807.
+#  
+# 
+#  示例 2： 
+# 
+#  
+# 输入：l1 = [0], l2 = [0]
+# 输出：[0]
+#  
+# 
+#  示例 3： 
+# 
+#  
+# 输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+# 输出：[8,9,9,9,0,0,0,1]
+#  
+# 
+#  
+# 
+#  提示： 
+# 
+#  
+#  每个链表中的节点数在范围 [1, 100] 内 
+#  0 <= Node.val <= 9 
+#  题目数据保证列表表示的数字不含前导零 
+#  
+# 
+#  Related Topics 递归 链表 数学 👍 10528 👎 0
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        # cur = dummy.next
+        cur = dummy
+        carry = 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            res = v1 + v2 + carry
+
+            cur.next = ListNode(res % 10)
+            carry = res // 10
+
+            # 容易遗漏的.
+            cur = cur.next
+            # 容易不判断空的情况.
+            # l1 = l1.next
+            # l2 = l2.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return dummy.next
+# leetcode submit region end(Prohibit modification and deletion)
