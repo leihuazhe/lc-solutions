d7f05dffe6b2a56d8e252f28faa464c7af65852e
leihuazhe <295482300@qq.com>
2024-06-08 20:09:48 +0800

    160-6: Reserved, time:  2024-06-08 20:09:47

--- python3/leetcode/editor/cn/1_TwoSum.py
+++ python3/leetcode/editor/cn/1_TwoSum.py
@@ -61,5 +61,22 @@ from typing import List
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums.sort()
+        l, r = 0, len(nums) - 1
+        while l < r:
+            s = nums[l] + nums[r]
+            if s < target:
+                l += 1
+            elif s > target:
+                r -= 1
+            else:
+                return [l, r]
+
         pass
+
+
 # leetcode submit region end(Prohibit modification and deletion)
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.twoSum([3, 2, 4], 6))
--- python3/leetcode/editor/en/42_trapping-rain-water.py
+++ python3/leetcode/editor/en/42_trapping-rain-water.py
@@ -44,6 +44,30 @@ from typing import List
 
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
+    """
+     基础方法
+     - 当前 index 假设为一个宽为1的桶，它能接水的多少，取决于它左边的木板的高度和右边的木板的高度，取最小值，然后减去当前 index 本身的高度。
+     - 实际是取决于**其左侧的最大高度** 和 **右侧的最大高度**。
+     - 使用两个额外数组,第一个数组存储从最左边到第i个位置的最大高度。即，前缀最大值。
+     - 第二个数组，存储从最右侧到 i 个位置的最大高度，及后缀最大值。
+     - 对于每个前缀最大值，可以用上一个前缀最大值和当前的高度取最大值，取当前最大值。
+- 前缀最大值 **`pre_max`** 和 后缀最大值 **`suf_max`**
+    """
+
+    def trapV1(self, height: List[int]) -> int:
+        ans = 0
+        n = len(height)
+        pre_max = [0] * n
+        suf_max = [0] * n
+        for i in range(1, n):
+            pre_max[i] = max(pre_max[i - 1], height[i])
+        for i in range(n - 2, -1, -1):
+            suf_max = max(suf_max[i + 1], height[i])
+
+        for pre, h, suf in zip(pre_max, height, suf_max):
+            ans += min(pre, suf) - h
+        return ans
+
     def trap(self, height: List[int]) -> int:
         if not height:
             return 0
