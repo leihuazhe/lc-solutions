d35f078302f0e6c27d76ba707c1951a9918b8af7
leihuazhe <295482300@qq.com>
2024-07-05 20:14:50 +0800

    187-5: Reserved, time:  2024-07-05 20:14:48

---
+++ src/main/java/com/shine/data/backtracking/Parentheses.java
@@ -0,0 +1,14 @@
+package com.shine.data.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parentheses {
+
+
+    public static void main(String[] args) {
+        List<String> result = new ArrayList<>();
+        result.removeLast();
+    }
+
+}
